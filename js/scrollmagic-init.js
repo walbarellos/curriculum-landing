7951e0287524424101aa6641200a14934996545f
Willian Albarello <willianalbarello@gmail.com>
2025-07-27 22:18:36 -0500

    🟢 update made - minor bugs fixed

---
+++ js/scrollmagic-init.js
@@ -0,0 +1,12 @@
+const controller = new ScrollMagic.Controller();
+["#atuacao", "#projetos", "#habilidades", "#certificados", "#contato"].forEach((section) => {
+  new ScrollMagic.Scene({
+    triggerElement: section,
+    triggerHook: 0.8,
+    reverse: false
+  })
+  .on("enter", function () {
+    gsap.to(section, { opacity: 1, y: 0, duration: 1, ease: "power2.out" });
+  })
+  .addTo(controller);
+});
